d53862720c7fe29c2a6ec6517dc4d241f8591a26
Eruin de Lima Silva <eruindelimasilva@MacBook-Air-de-Eruin.local>
2022-08-25 15:11:14 -0300

    layout pagina index

--- var/cache/dev/twig/c0/c03cee9e76e321e1b8defe88b0012320.php
+++ var/cache/dev/twig/c0/c03cee9e76e321e1b8defe88b0012320.php
@@ -85,24 +85,51 @@ class __TwigTemplate_de914361ebee193e7525c3cca9ff6e89 extends Template
         $__internal_6f47bbe9983af81f1e7450e9a3e3768f->enter($__internal_6f47bbe9983af81f1e7450e9a3e3768f_prof = new \Twig\Profiler\Profile($this->getTemplateName(), "block", "body"));
 
         // line 6
-        echo "    ";
+        echo "
+
+    ";
+        // line 8
         $context['_parent'] = $context;
-        $context['_seq'] = twig_ensure_traversable((isset($context["produtos"]) || array_key_exists("produtos", $context) ? $context["produtos"] : (function () { throw new RuntimeError('Variable "produtos" does not exist.', 6, $this->source); })()));
+        $context['_seq'] = twig_ensure_traversable((isset($context["produtos"]) || array_key_exists("produtos", $context) ? $context["produtos"] : (function () { throw new RuntimeError('Variable "produtos" does not exist.', 8, $this->source); })()));
         $context['_iterated'] = false;
         foreach ($context['_seq'] as $context["_key"] => $context["produto"]) {
-            // line 7
-            echo "        ";
-            echo twig_escape_filter($this->env, twig_get_attribute($this->env, $this->source, $context["produto"], "id", [], "any", false, false, false, 7), "html", null, true);
-            echo "
-        ";
-            // line 8
-            echo twig_escape_filter($this->env, twig_get_attribute($this->env, $this->source, $context["produto"], "nome", [], "any", false, false, false, 8), "html", null, true);
-            echo "
+            // line 9
+            echo "    <div class=\"container\">
+    <div class=\"row\">
+    <div class=\"col\">
+    <div class=\"card\" style=\"width: 18rem;\">
+        <img src=\"...\" class=\"card-img-top\" alt=\"...\">
+        <div class=\"card-body\">
+            <h5 class=\"card-title\">";
+            // line 15
+            echo twig_escape_filter($this->env, twig_get_attribute($this->env, $this->source, $context["produto"], "nome", [], "any", false, false, false, 15), "html", null, true);
+            echo "</h5>
+            <p class=\"card-text\">";
+            // line 16
+            echo twig_escape_filter($this->env, twig_get_attribute($this->env, $this->source, $context["produto"], "descricao", [], "any", false, false, false, 16), "html", null, true);
+            echo "</p>
+        </div>
+        <ul class=\"list-group list-group-flush\">
+            <li class=\"list-group-item\">Valor R\$: ";
+            // line 19
+            echo twig_escape_filter($this->env, twig_get_attribute($this->env, $this->source, $context["produto"], "preco", [], "any", false, false, false, 19), "html", null, true);
+            echo "</li>
+            <li class=\"list-group-item\">Categoria do produto </li>
+            <li class=\"list-group-item\">Caracteristicas do Produto  </li>
+        </ul>
+        <div class=\"card-body\">
+            <a href=\"#\" class=\"card-link btn btn-outline-primary\">Adicionar ao carrinho</a>
+        </div>
+    </div>
+    </div>
+    </div>
+    </div>
+    
     ";
             $context['_iterated'] = true;
         }
         if (!$context['_iterated']) {
-            // line 10
+            // line 32
             echo "        no records found
     ";
         }
@@ -129,7 +156,7 @@ class __TwigTemplate_de914361ebee193e7525c3cca9ff6e89 extends Template
 
     public function getDebugInfo()
     {
-        return array (  106 => 10,  99 => 8,  94 => 7,  88 => 6,  78 => 5,  59 => 3,  36 => 1,);
+        return array (  133 => 32,  115 => 19,  109 => 16,  105 => 15,  97 => 9,  92 => 8,  88 => 6,  78 => 5,  59 => 3,  36 => 1,);
     }
 
     public function getSourceContext()
@@ -139,9 +166,31 @@ class __TwigTemplate_de914361ebee193e7525c3cca9ff6e89 extends Template
 {% block title %}Hello StoreController!{% endblock %}
 
 {% block body %}
+
+
     {% for produto in produtos %}
-        {{ produto.id }}
-        {{ produto.nome }}
+    <div class=\"container\">
+    <div class=\"row\">
+    <div class=\"col\">
+    <div class=\"card\" style=\"width: 18rem;\">
+        <img src=\"...\" class=\"card-img-top\" alt=\"...\">
+        <div class=\"card-body\">
+            <h5 class=\"card-title\">{{ produto.nome }}</h5>
+            <p class=\"card-text\">{{ produto.descricao }}</p>
+        </div>
+        <ul class=\"list-group list-group-flush\">
+            <li class=\"list-group-item\">Valor R\$: {{ produto.preco }}</li>
+            <li class=\"list-group-item\">Categoria do produto </li>
+            <li class=\"list-group-item\">Caracteristicas do Produto  </li>
+        </ul>
+        <div class=\"card-body\">
+            <a href=\"#\" class=\"card-link btn btn-outline-primary\">Adicionar ao carrinho</a>
+        </div>
+    </div>
+    </div>
+    </div>
+    </div>
+    
     {% else %}
         no records found
     {% endfor %}
